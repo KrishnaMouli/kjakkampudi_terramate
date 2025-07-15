63e4f2ebcbddd4ec42b5ba8f26ca66854fc47e6a
root <root@ip-172-31-86-45.ec2.internal>
2025-07-15 11:49:56 +0000

    commit

--- s3/main.tf
+++ s3/main.tf
@@ -1,15 +1,8 @@
 resource "aws_s3_bucket" "example" {
   bucket = "dev-dit"
-  object_lock_enabled = var.object_locking
 
   tags = {
     Name        = "My bucket"
   }
 }
 
-
-variable "object_locking" {
-  type = bool
-
-
-}
