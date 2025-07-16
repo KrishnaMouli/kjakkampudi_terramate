48c48f5d43b6b48015dcc054848e0ea0ff8847dd
root <root@ip-172-31-25-130.ec2.internal>
2025-07-16 07:02:10 +0000

    Testing git flow

--- dev/s3.tf
+++
@@ -1,5 +0,0 @@
-// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
-
-module "s3" {
-  source = "../s3"
-}
--- s3/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "dev-dit"
-
-  tags = {
-    Name        = "My bucket"
-  }
-}
-
